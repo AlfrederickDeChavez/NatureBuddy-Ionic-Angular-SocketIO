649a0416796dc0aab61498fd52de260f8241faae
AlfrederickDeChavez <dechavezalfrederick@gmail.com>
2022-08-04 00:52:02 +0800

    Prototype for presentation

--- server/server.js
+++ server/server.js
@@ -13,25 +13,38 @@ const io = new Server(server, {
     }
 })
 
-
-
 io.on("connection", (socket) => {
-    socket.on('login', username=> {
-        socket.emit('user-joined', username)
-        console.log(username)
 
+    socketInfo = { id: socket.id, username: ''}
+    console.log('User connected.')
+
+    //User has logged in.
+    socket.on('login', username => {
+        socket.emit('user-joined', username)
+        socketInfo.username = username
+        console.log(socketInfo)
+
+        //User has post an action --> feeds
         socket.on('post-action', data => {
             console.log(data)
             io.emit('receive-post', data)
-        })
+        })  
+
+        
+    })   
+
+    //User has start a project.
+    socket.on('start-project', project => {
+        socket.broadcast.emit('receive-project', project)
+        console.log(project)
     })
-
     
-})
 
+})
 
 var PORT = process.env.PORT || 3001
 
 server.listen(PORT, () => {
     console.log(`SERVER RUNNING AT PORT ${PORT}`)
-} )
+})
+
